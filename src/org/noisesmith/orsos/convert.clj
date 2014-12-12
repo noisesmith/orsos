0a35a2cead64d51a9a043bf28fe5b2a3722983ec
Justin Smith <noisesmith@gmail.com>
2014-12-12 12:14:16 -0800

    updated data import, better number support, enums

--- src/org/noisesmith/orsos/convert.clj
+++ src/org/noisesmith/orsos/convert.clj
@@ -5,34 +5,52 @@
 (defn get-instant
   [ds]
   (let [[m d y] (string/split ds #"/")]
-    (instant/read-instant-date (str y \- m \- d))))
+      (instant/read-instant-date (str y \- m \- d))))
 
-(def parsers
+(defn parse-num
+  [s]
+  (try
+    (-> s
+        (.replaceAll "(?i)zero" "0")
+        (.replaceAll "(?i)none" "0")
+        (.replaceAll "(?i)o" "0")
+        (->> (re-find #"[0-9\.]+"))
+        BigDecimal.)
+    (catch Exception _
+      (println "bad num" s)
+      0M)))
+
+#(try (BigDecimal. %) (catch Exception _ 0M))
+(defn parsers
   "A map from the keyword in our datomic schema to the function which will
   generate data of that type from the value we will get from the CSV."
-  {:string identity
-   :instant get-instant
-   :enum (comp keyword #(.toLowerCase %))
-   :bigdec #(BigDecimal. (or (not-empty %) "0"))
-   :boolean {"1" true
-             "0" false
-             "true" true
-             "yes" true
-             "false" false
-             "no" false}})
+  [[from spec space field]]
+  (println "parse" (pr-str from) (pr-str spec))
+  (get 
+   {:string identity
+    :instant get-instant
+    :enum #(keyword (str space \. field) (.toLowerCase %))
+    :bigdec parse-num
+    :boolean {"1" true
+              "0" false
+              "true" true
+              "yes" true
+              "false" false
+              "no" false}}
+   from
+   identity))
 
 (defn get-type
   "Use our datomic schema data structure to find the data converter we need."
   [schema table field]
   (-> schema
-      (->> (group-by :name))
-      (get (name table))
-      (get 0)
+      (->> (filter #(= (:name %) (name table))))
+      first
       :fields
       (get (name field))
-      first
-      ;; (doto pprint)
-      (parsers identity)))
+      (conj (name table))
+      (conj (name field))
+      parsers))
 
 (defn make-index
   "Generate a lookup table from the fields we want to their position in the
