689572b90a333fa6cc1550526e924198e2d52450
Justin Smith <noisesmith@gmail.com>
2014-12-11 18:46:34 -0800

    comments, minor reorg

--- src/org/noisesmith/orsos/convert.clj
+++ src/org/noisesmith/orsos/convert.clj
@@ -8,6 +8,8 @@
     (instant/read-instant-date (str y \- m \- d))))
 
 (def parsers
+  "A map from the keyword in our datomic schema to the function which will
+  generate data of that type from the value we will get from the CSV."
   {:string identity
    :instant get-instant
    :enum (comp keyword #(.toLowerCase %))
@@ -20,6 +22,7 @@
              "no" false}})
 
 (defn get-type
+  "Use our datomic schema data structure to find the data converter we need."
   [schema table field]
   (-> schema
       (->> (group-by :name))
@@ -32,6 +35,9 @@
       (parsers identity)))
 
 (defn make-index
+  "Generate a lookup table from the fields we want to their position in the
+  CSV input, with the help of lookup, which maps from the heading in the CSV
+  to the keyword for the field in the schema."
   [lookup fields]
   (reduce (fn [m [n k]]
             (if-let [ky (get lookup k)]
--- src/org/noisesmith/orsos/load.clj
+++ src/org/noisesmith/orsos/load.clj
@@ -7,24 +7,30 @@
             [clojure.data.csv :as csv]))
 
 (def transaction-dir
+  "Directory that holds the cash transaction CSV files."
   "orsos/transactions/")
 
 (defn load-csv
   [path]
+  "Load an individual CSV file from the classpath."
   (with-open [source (io/reader (io/resource path))]
     (doall (csv/read-csv source))))
 
 (def load-transactions
+  "Get all the contents from all the cash transaction CSV files in a given
+  directory.
+  Memoized for development purposes."
   (memoize
-   (fn []
-     (let [dir (.list (io/file (io/resource transaction-dir)))]
-       (map #(load-csv (str transaction-dir \/ %)) dir)))))
+   (fn [from-dir]
+     (let [dir (.list (io/file (io/resource from-dir)))]
+       (map #(load-csv (str from-dir \/ %)) dir)))))
 
 
 (defn build-transaction
+  "Generate the :db/add clauses to insert one row from a CSV."
   [index-lookup]
   (fn builder [tran]
-    (let [id (datomic/tempid :db.part/user)]
+    (let [id (datomic/tempid :db.part/user)] ;; every insert is to the same id
       (reduce (fn [v [k idx]]
                 (let [typer (convert/get-type schema/orsos-schema
                                               "transaction" k)
@@ -34,18 +40,27 @@
               index-lookup))))
 
 (defn build-transactions
+  "Generate input suitable for the datomic database from a CSV file."
+  [trans]
+  (let [fields (first trans)
+        lookup schema/transaction-lookup
+        index-lookup (convert/make-index lookup fields)]
+    (mapcat (build-transaction index-lookup)
+            (take 2 (rest trans)))))
+
+(defn transaction-runner
+  "Construct an executor for transactions with this connection."
   [conn]
-  (fn [trans]
-    (let [fields (first trans)
-          lookup schema/transaction-lookup
-          index-lookup (convert/make-index lookup fields)
-          trans-data (mapcat (build-transaction index-lookup)
-                             (take 2 (rest trans)))]
-      (deref (datomic/transact conn trans-data)))))
+  (fn run-transaction
+    ;; Run a transaction for the the data supplied.
+    [data]
+    (deref (datomic/transact conn (build-transactions data)))))
 
 (defn load-all
-  [conn]
-  "populate the in-memory datomic database with the downloaded csv data"
-  (let [orso-transactions (load-transactions)]
-    (mapv (build-transactions conn)
+  "Populate the in-memory datomic database with the downloaded csv data."
+  [conn & {transaction-directory :transactions
+           :as opts
+           :or {transaction-directory transaction-dir}}]
+  (let [orso-transactions (load-transactions transaction-dir)]
+    (mapv (transaction-runner conn)
           (take 2 orso-transactions))))
