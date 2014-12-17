dd88f65c000fe21f4a5dbdd81f79df9ba0e281c5
Justin Smith <noisesmith@gmail.com>
2014-12-17 09:57:32 -0800

    updated clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0" #_"1.7.0-alpha4"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
                  [com.datomic/datomic-free "0.9.5078"]
                  [datomic-schema "1.1.0"]
                  [org.clojure/data.csv "0.1.2"]]
